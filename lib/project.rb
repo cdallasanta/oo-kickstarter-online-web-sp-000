03675e272ace004c99b18743f47eab3cca92c73e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-08 08:13:44 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
-  attr_accessor :name, :backers
+  attr_accessor :title, :backers
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
   end
 end
